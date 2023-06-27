86a631bfe79a64e6cf030a58505d7c3675dc9cfa
A. Roig <antiaroig@gmail.com>
2023-06-27 17:19:39 +0200

    A* pathfinding

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -1,27 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Target : MonoBehaviour {
 
-    private Vector2 followSpot;
     [SerializeField] private float speed;
 
+
     void Start() {
-        followSpot = transform.position;
+        
     }
 
     void Update() {
         Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         if (Input.GetMouseButtonDown(0)) {
-            followSpot = mousePosition;
+            transform.position = mousePosition;
         }
-
-        transform.position = Vector2.MoveTowards(transform.position, followSpot, speed * Time.deltaTime);
-    }
-
-    void OnCollisionStay2D(Collision2D other)
-    {
-        followSpot = transform.position;
     }
 }
