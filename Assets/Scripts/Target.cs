02836c517155ffc97da8aed217fc29bbae637ea2
A. Roig <antiaroig@gmail.com>
2023-06-27 19:12:41 +0200

    Correccións

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -2,13 +2,6 @@ using UnityEngine;
 
 public class Target : MonoBehaviour {
 
-    [SerializeField] private float speed;
-
-
-    void Start() {
-        
-    }
-
     void Update() {
         Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         if (Input.GetMouseButtonDown(0)) {
